e002d5e4bfdb389c5492b18a1d4a9b9e2b07f2d7
Lazare Chkhartishvili <96971788+Lazzzare@users.noreply.github.com>
2023-11-21 00:27:46 +0400

    Create QuizHeader.vue

---
+++ src/components/QuizHeader.vue
@@ -0,0 +1,22 @@
+<template>
+  <header class="mt-10 text-3xl">
+    <h4>Question 1/3</h4>
+    <div class="bar">
+      <div class="completion"></div>
+    </div>
+  </header>
+</template>
+
+<style scoped>
+.bar {
+  width: 300px;
+  height: 50px;
+  border: 3px solid bisque;
+}
+
+.completion {
+  height: 100%;
+  width: 0%;
+  background-color: bisque;
+}
+</style>
