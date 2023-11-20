8ede0df5750dd8c725a19fe16a0e6ca72941c44b
Lazare Chkhartishvili <96971788+Lazzzare@users.noreply.github.com>
2023-11-20 17:05:52 +0400

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,33 @@
-<script setup></script>
+<script setup>
+import Card from "./components/Card.vue";
+import q from "./data/quizes.json";
+import { ref, watch } from "vue";
+
+const quizes = ref(q);
+const search = ref("");
+
+watch(search, () => {
+  quizes.value = q.filter((quiz) =>
+    quiz.name.toLowerCase().includes(search.value.toLowerCase)
+  );
+});
+</script>
 
 <template>
   <div class="mx-auto max-w-[1000px]">
     <header class="pt-20 flex flex-row items-center gap-20">
-      <h1 class="text-4xl font-bold">Quizes</h1>
+      <h1 class="text-4xl font-bold cursor-pointer">Quizes</h1>
       <input
+        v-model.trim()="search"
         type="text"
         placeholder="Search..."
         class="border border-black py-2 px-5 bg-slate-100 placeholder:text-black rounded-md focus:outline-none focus:scale-105 transition-all duration-300"
       />
     </header>
-    <!-- Card -->
-    <div
-      class="flex flex-col rounded-xl bg-gray-300 w-[300px] mt-16 transition-all duration-500 hover:scale-105 cursor-pointer"
-    >
-      <img
-        src="/src/assets/front.png"
-        alt="frontEndImage"
-        class="w-[300px] h-[250px] rounded-t-xl"
-      />
-      <div class="flex flex-col gap-5 p-5">
-        <h1 class="text-lg font-bold">Front-end Development</h1>
-        <p class="font-bold text-gray-500">15 Questions</p>
-      </div>
+    <!-- Cards -->
+    <div class="flex flex-row gap-20">
+      <!-- Card -->
+      <Card v-for="quiz in quizes" :key="quiz.id" :quiz="quiz" />
     </div>
   </div>
 </template>
