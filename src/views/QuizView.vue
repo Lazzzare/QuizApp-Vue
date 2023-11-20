88b91b9e293daab5b58c317a865ae95b69ee876a
Lazare Chkhartishvili <96971788+Lazzzare@users.noreply.github.com>
2023-11-21 00:27:54 +0400

    Create QuizView.vue

---
+++ src/views/QuizView.vue
@@ -0,0 +1,19 @@
+<script setup>
+import Question from "../components/Question.vue";
+import QuizHeader from "../components/QuizHeader.vue";
+import { useRoute } from "vue-router";
+import { ref } from "vue";
+import quizes from "../data/quizes.json";
+const route = useRoute();
+
+const quizId = parseInt(route.params.id);
+const quiz = quizes.find((q) => q.id === quizId);
+const currentQuestionIndex = ref(0);
+</script>
+
+<template>
+  <div>
+    <QuizHeader />
+    <Question :question="quiz.questions[currentQuestionIndex]" />
+  </div>
+</template>
