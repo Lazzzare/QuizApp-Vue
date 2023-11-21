281d0e594094b214f0917d7b527c26b61d6c5ad9
Lazare Chkhartishvili <96971788+Lazzzare@users.noreply.github.com>
2023-11-21 19:24:21 +0400

    Update QuizView.vue

--- src/views/QuizView.vue
+++ src/views/QuizView.vue
@@ -2,18 +2,23 @@
 import Question from "../components/Question.vue";
 import QuizHeader from "../components/QuizHeader.vue";
 import { useRoute } from "vue-router";
-import { ref } from "vue";
+import { ref, watch, computed } from "vue";
 import quizes from "../data/quizes.json";
-const route = useRoute();
 
+const route = useRoute();
 const quizId = parseInt(route.params.id);
 const quiz = quizes.find((q) => q.id === quizId);
 const currentQuestionIndex = ref(0);
+
+const questionStatus = computed(() => {
+  return `${currentQuestionIndex.value} / ${quiz.questions.length}`;
+});
 </script>
 
 <template>
   <div>
-    <QuizHeader />
+    <QuizHeader :questionStatus="questionStatus" />
     <Question :question="quiz.questions[currentQuestionIndex]" />
+    <button @click="currentQuestionIndex++">Next Question</button>
   </div>
 </template>
